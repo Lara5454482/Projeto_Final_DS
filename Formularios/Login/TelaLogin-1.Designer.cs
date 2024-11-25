d37f3d99983fae18a05c4689f4ed193e6bd80c94
Aluno <Aluno@LS05M03>
2024-11-25 13:30:07 -0300

    principal e usuario

--- Formularios/Login/TelaLogin-1.Designer.cs
+++ Formularios/Login/TelaLogin-1.Designer.cs
@@ -40,26 +40,26 @@ namespace Projeto_Final.Formularios
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.Font = new System.Drawing.Font("Arial", 12F);
             this.label3.Location = new System.Drawing.Point(29, 197);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(56, 20);
+            this.label3.Size = new System.Drawing.Size(53, 18);
             this.label3.TabIndex = 17;
             this.label3.Text = "Senha";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.Font = new System.Drawing.Font("Arial", 12F);
             this.label2.Location = new System.Drawing.Point(34, 106);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(48, 20);
+            this.label2.Size = new System.Drawing.Size(47, 18);
             this.label2.TabIndex = 16;
             this.label2.Text = "Login";
             // 
             // btnEntrar
             // 
-            this.btnEntrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.btnEntrar.Font = new System.Drawing.Font("Arial", 12F);
             this.btnEntrar.Location = new System.Drawing.Point(150, 293);
             this.btnEntrar.Name = "btnEntrar";
             this.btnEntrar.Size = new System.Drawing.Size(149, 49);
@@ -69,7 +69,7 @@ namespace Projeto_Final.Formularios
             // 
             // txtSenha
             // 
-            this.txtSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.txtSenha.Font = new System.Drawing.Font("Arial", 12F);
             this.txtSenha.Location = new System.Drawing.Point(32, 220);
             this.txtSenha.Multiline = true;
             this.txtSenha.Name = "txtSenha";
@@ -79,7 +79,7 @@ namespace Projeto_Final.Formularios
             // 
             // txtLogin
             // 
-            this.txtLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.txtLogin.Font = new System.Drawing.Font("Arial", 12F);
             this.txtLogin.Location = new System.Drawing.Point(32, 138);
             this.txtLogin.Multiline = true;
             this.txtLogin.Name = "txtLogin";
@@ -89,12 +89,12 @@ namespace Projeto_Final.Formularios
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
-            this.label1.Location = new System.Drawing.Point(171, 33);
+            this.label1.Font = new System.Drawing.Font("Arial", 18F);
+            this.label1.Location = new System.Drawing.Point(130, 27);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(99, 31);
+            this.label1.Size = new System.Drawing.Size(177, 27);
             this.label1.TabIndex = 12;
-            this.label1.Text = "LOGIN";
+            this.label1.Text = "Faça seu Login";
             // 
             // frmUsuarios
             // 
@@ -109,6 +109,7 @@ namespace Projeto_Final.Formularios
             this.Controls.Add(this.label1);
             this.Name = "frmUsuarios";
             this.Text = "frmUsuarios";
+            this.Load += new System.EventHandler(this.frmUsuarios_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
